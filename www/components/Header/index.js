048c4851bb6c6654b4bde5488f00ed2e9f13ebb2
Adam Soffer <adam@soffer.dev>
2019-04-19 15:55:37 -0400

    Update header

--- www/components/Header/index.js
+++ www/components/Header/index.js
@@ -1,19 +1,8 @@
 import React from 'react'
-import styled from '@emotion/styled'
-import { palette, spacing } from '@material-ui/system'
-// import NoSsr from '@material-ui/core/NoSsr';
+import { Root } from './styles'
 
-const Box = styled.div`
-  ${palette}
-  ${spacing}
-`
-
-function Header() {
-  return (
-    <Box color="white" bgcolor="palevioletred" p={1}>
-      Emotion test
-    </Box>
-  )
-}
-
-export default Header
+export default () => (
+  <Root color="white" bgcolor="palevioletred" p={1}>
+    Emotion test
+  </Root>
+)
---
+++ www/components/Header/styles.js
@@ -0,0 +1,7 @@
+import { palette, spacing } from '@material-ui/system'
+import styled from '@emotion/styled'
+
+export const Root = styled.div({
+  ...palette,
+  ...spacing
+})
