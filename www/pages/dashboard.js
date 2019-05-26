af720effe9186328da0274c014994db1d6cd3e08
Adam Chambers <adammchambers@gmail.com>
2019-05-26 12:57:53 -0400

    🚧 Check user signed in on dashboard page, if not redirect to home

--- www/pages/dashboard.js
+++ www/pages/dashboard.js
@@ -2,9 +2,34 @@ import { useState } from 'react'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
+import Router from 'next/router'
 import Link from 'next/link'
 import Layout from '../layouts'
 
 export default () => {
-  return <Layout>Dashboard</Layout>
+  if (!firebase.apps.length) {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAwFqYtlB34EoaRXHcmAR_U90ChT0vRxUs',
+      authDomain: 'goodform-d0096.firebaseapp.com',
+      databaseURL: 'https://goodform-d0096.firebaseio.com',
+      projectId: 'goodform-d0096',
+      storageBucket: 'goodform-d0096.appspot.com',
+      messagingSenderId: '1090475289248',
+      appId: '1:1090475289248:web:9464f7c31608f743'
+    })
+  }
+
+  firebase.auth().onAuthStateChanged(user => {
+    if (!user) {
+      Router.push('/')
+    }
+  })
+  return (
+    <Layout>
+      <h1>Dashboard</h1>
+      <Link href="/create">
+        <a>Create Form</a>
+      </Link>
+    </Layout>
+  )
 }
