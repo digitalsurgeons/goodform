6e9e02912e4732fd0e48da8116fd51c5e46cc107
Adam Chambers <adammchambers@gmail.com>
2019-05-26 12:15:44 -0400

    🚧 Scaffold create page with basic form logging data

--- www/pages/create.js
+++ www/pages/create.js
@@ -2,9 +2,37 @@ import { useState } from 'react'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
+import Container from '@material-ui/core/Container'
 import Link from 'next/link'
 import Layout from '../layouts'
 
 export default () => {
-  return <Layout>Dashboard</Layout>
+  const [formName, setFormname] = useState('')
+  const [emailList, setEmailList] = useState('')
+
+  function submit(e) {
+    e.preventDefault()
+
+    console.log(formName, emailList)
+  }
+
+  return (
+    <Layout>
+      <Container>
+        <form onSubmit={submit}>
+          <p>Form Name</p>
+          <input
+            type="text"
+            onKeyUp={e => setFormname(e.currentTarget.value)}
+          />
+          <p>Send emails to</p>
+          <input
+            type="text"
+            onKeyUp={e => setEmailList(e.currentTarget.value)}
+          />
+          <input type="submit" value="Create Form" />
+        </form>
+      </Container>
+    </Layout>
+  )
 }
