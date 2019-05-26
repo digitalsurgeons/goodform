b0f12dea13842e9d310690050b4d9f800f242468
Adam Chambers <adammchambers@gmail.com>
2019-05-26 12:58:09 -0400

    🚧 Check if user is logged in on home page and redirect to dashboard

--- www/pages/index.js
+++ www/pages/index.js
@@ -1,6 +1,27 @@
+import Router from 'next/router'
 import Layout from '../layouts'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 
 function Index() {
+  if (!firebase.apps.length) {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAwFqYtlB34EoaRXHcmAR_U90ChT0vRxUs',
+      authDomain: 'goodform-d0096.firebaseapp.com',
+      databaseURL: 'https://goodform-d0096.firebaseio.com',
+      projectId: 'goodform-d0096',
+      storageBucket: 'goodform-d0096.appspot.com',
+      messagingSenderId: '1090475289248',
+      appId: '1:1090475289248:web:9464f7c31608f743'
+    })
+  }
+
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      Router.push('/dashboard')
+    }
+  })
+
   return <Layout />
 }
 
